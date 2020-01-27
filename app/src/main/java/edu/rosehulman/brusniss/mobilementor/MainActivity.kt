58247fee138b6a606cd97f34209c772c47f7327a
Rias A Hilliard <hilliara@rose-hulman.edu>
2020-01-27 16:43:09 -0500

    Remove default fragment implementations

--- app/src/main/java/edu/rosehulman/brusniss/mobilementor/MainActivity.kt
+++ app/src/main/java/edu/rosehulman/brusniss/mobilementor/MainActivity.kt
@@ -36,9 +36,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send, R.id.nav_profile,
-                R.id.nav_pub_forum
+                R.id.nav_profile, R.id.nav_pub_forum
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)

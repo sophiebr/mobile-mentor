a86bf5c9a7a7c70b42442ec819501b79ba26af54
Rias A Hilliard <hilliara@rose-hulman.edu>
2020-01-27 16:38:50 -0500

    Add Public forum fragment and link up its navigation

--- app/src/main/java/edu/rosehulman/brusniss/mobilementor/MainActivity.kt
+++ app/src/main/java/edu/rosehulman/brusniss/mobilementor/MainActivity.kt
@@ -37,7 +37,8 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send, R.id.nav_profile
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send, R.id.nav_profile,
+                R.id.nav_pub_forum
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)

eec7d205f6dcf57993a99859dc209dbda8368a03
Rias A Hilliard <hilliara@rose-hulman.edu>
2020-01-27 16:30:04 -0500

    Profile Fragment inflates navigation view

--- app/src/main/java/edu/rosehulman/brusniss/mobilementor/profile/ProfileFragment.kt
+++ app/src/main/java/edu/rosehulman/brusniss/mobilementor/profile/ProfileFragment.kt
@@ -1,6 +1,22 @@
 package edu.rosehulman.brusniss.mobilementor.profile
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import edu.rosehulman.brusniss.mobilementor.R
+import edu.rosehulman.brusniss.mobilementor.ui.gallery.GalleryViewModel
 
 class ProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
 }

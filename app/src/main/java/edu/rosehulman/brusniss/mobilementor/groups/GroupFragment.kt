97c056f5647e93cbc8adee4bb03e903d28aeb774
Rias A Hilliard <hilliara@rose-hulman.edu>
2020-01-27 16:51:14 -0500

    Add missing group package from last commit

---
+++ app/src/main/java/edu/rosehulman/brusniss/mobilementor/groups/GroupFragment.kt
@@ -0,0 +1,18 @@
+package edu.rosehulman.brusniss.mobilementor.groups
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import edu.rosehulman.brusniss.mobilementor.R
+
+class GroupFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_gradient_background, container, false)
+    }
+}

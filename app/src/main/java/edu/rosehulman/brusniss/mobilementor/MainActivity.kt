1522efc48f9d5c5520abbbf8c061f062f5f340f9
Rias A Hilliard <hilliara@rose-hulman.edu>
2020-01-27 16:31:50 -0500

    Add nav_profile to AppBarConfiguration

--- app/src/main/java/edu/rosehulman/brusniss/mobilementor/MainActivity.kt
+++ app/src/main/java/edu/rosehulman/brusniss/mobilementor/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send, R.id.nav_profile
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)

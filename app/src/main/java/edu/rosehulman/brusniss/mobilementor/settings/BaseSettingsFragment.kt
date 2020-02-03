30514cab1e64eb9298b6e0e83fafc3834e70abf5
Rias A Hilliard <hilliara@rose-hulman.edu>
2020-02-03 08:08:07 -0500

    Change Settings fragment to use PreferenceFragmentCompat

--- app/src/main/java/edu/rosehulman/brusniss/mobilementor/settings/BaseSettingsFragment.kt
+++ app/src/main/java/edu/rosehulman/brusniss/mobilementor/settings/BaseSettingsFragment.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
 import edu.rosehulman.brusniss.mobilementor.R
 
-class BaseSettingsFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_gradient_background, container, false)
+class BaseSettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
     }
+
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        return inflater.inflate(R.layout.fragment_gradient_background, container, false)
+//    }
 }

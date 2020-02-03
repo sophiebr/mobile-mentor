680520dc3990e726f4f22050109bec8db1bce565
Rias A Hilliard <hilliara@rose-hulman.edu>
2020-02-03 09:27:03 -0500

    ForumPostFragment uses adapter for responses

--- app/src/main/java/edu/rosehulman/brusniss/mobilementor/forum/ForumPostFragment.kt
+++ app/src/main/java/edu/rosehulman/brusniss/mobilementor/forum/ForumPostFragment.kt
@@ -5,12 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import edu.rosehulman.brusniss.mobilementor.R
 import kotlinx.android.synthetic.main.fragment_forum_post.view.*
+import kotlinx.android.synthetic.main.fragment_gradient_background.view.*
 
 class ForumPostFragment : Fragment() {
 
-    var forumPostModel: ForumPostModel? = null
+    private var forumPostModel: ForumPostModel? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,6 +26,10 @@ class ForumPostFragment : Fragment() {
         val forumView = inflater.inflate(R.layout.fragment_forum_post, container, false)
         forumView.post_question_header.post_title_text.text = forumPostModel?.title
         // More initialization from the forum post model...
+        forumView.post_response_recycler_view.layoutManager = LinearLayoutManager(context)
+        val adapter = ForumResponseAdapter(context!!, forumPostModel!!.responses)
+        forumView.post_response_recycler_view.setHasFixedSize(true)
+        forumView.post_response_recycler_view.adapter = adapter
         return forumView
     }
 }

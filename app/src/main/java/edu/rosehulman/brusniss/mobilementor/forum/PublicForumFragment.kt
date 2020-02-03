4acf8eb50de53deb62fe701b2183d27f07c62bbe
Rias A Hilliard <hilliara@rose-hulman.edu>
2020-02-03 08:42:59 -0500

    Forum post fragment loads and uses adapter

--- app/src/main/java/edu/rosehulman/brusniss/mobilementor/forum/PublicForumFragment.kt
+++ app/src/main/java/edu/rosehulman/brusniss/mobilementor/forum/PublicForumFragment.kt
@@ -5,14 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import edu.rosehulman.brusniss.mobilementor.R
+import kotlinx.android.synthetic.main.fragment_gradient_background.view.*
 
 class PublicForumFragment : Fragment() {
+    
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_gradient_background, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_gradient_background, container, false)
+        setHasOptionsMenu(true)
+        rootView.gradient_recycler_view.layoutManager = LinearLayoutManager(context)
+        val adapter = ForumPostListAdapter(context!!, findNavController(), rootView.gradient_recycler_view.layoutManager as LinearLayoutManager)
+        rootView.gradient_recycler_view.setHasFixedSize(true)
+        rootView.gradient_recycler_view.adapter = adapter
+        return rootView
     }
 }

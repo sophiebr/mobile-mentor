e6325f9f700516c813f61f029e9e61bf40e17aaa
Rias A Hilliard <hilliara@rose-hulman.edu>
2020-02-03 09:01:49 -0500

    ForumPostFragment uses a ForumModel from its arguments

--- app/src/main/java/edu/rosehulman/brusniss/mobilementor/forum/ForumPostFragment.kt
+++ app/src/main/java/edu/rosehulman/brusniss/mobilementor/forum/ForumPostFragment.kt
@@ -9,11 +9,16 @@ import edu.rosehulman.brusniss.mobilementor.R
 
 class ForumPostFragment : Fragment() {
 
+    var forumPostModel: ForumPostModel? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        arguments?.let {
+            forumPostModel = it.getParcelable("post")
+        }
         return inflater.inflate(R.layout.fragment_forum_post, container, false)
     }
 }

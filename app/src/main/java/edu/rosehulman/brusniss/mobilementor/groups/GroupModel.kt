a8472a60175d8cdf498d45760f6d63c9809b91cc
Rias A Hilliard <hilliara@rose-hulman.edu>
2020-02-09 16:14:22 -0500

    Add group code to Group model

--- app/src/main/java/edu/rosehulman/brusniss/mobilementor/groups/GroupModel.kt
+++ app/src/main/java/edu/rosehulman/brusniss/mobilementor/groups/GroupModel.kt
@@ -5,4 +5,4 @@ import edu.rosehulman.brusniss.mobilementor.forum.ForumPostModel
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class GroupModel(var groupName: String = "", var forumPosts: List<ForumPostModel> = ArrayList<ForumPostModel>()) : Parcelable
+data class GroupModel(var groupName: String = "", var code: Int = 0, var forumPosts: List<ForumPostModel> = ArrayList<ForumPostModel>()) : Parcelable

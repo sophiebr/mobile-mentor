bdcbdeed1aed2092352fc0fff679789f35f875df
Rias A Hilliard <hilliara@rose-hulman.edu>
2020-02-14 14:13:11 -0500

    Moved all login code to LoginFragment

--- app/src/main/java/edu/rosehulman/brusniss/mobilementor/MainActivity.kt
+++ app/src/main/java/edu/rosehulman/brusniss/mobilementor/MainActivity.kt
@@ -4,14 +4,11 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -19,17 +16,12 @@ import android.view.Menu
 import android.view.MenuItem
 import com.google.firebase.auth.FirebaseAuth
 import edu.rosehulman.brusniss.mobilementor.forum.PublicForumFragment
-import edu.rosehulman.brusniss.mobilementor.login.SplashFragment
+import edu.rosehulman.brusniss.mobilementor.login.LoginFragment
 import edu.rosehulman.rosefire.Rosefire
-import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : AppCompatActivity(),
-    SplashFragment.OnRosefireLoginButtonPressedListener{
+class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private val auth = FirebaseAuth.getInstance()
-    lateinit var authStateListener: FirebaseAuth.AuthStateListener
-    private val RC_ROSEFIRE_LOGIN = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,50 +42,6 @@ class MainActivity : AppCompatActivity(),
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-
-        initializeListeners()
-    }
-
-    private fun initializeListeners() {
-        authStateListener = FirebaseAuth.AuthStateListener { auth: FirebaseAuth ->
-            val user = auth.currentUser
-            Log.d(Constants.TAG, "In auth listener, user = $user")
-            if (user != null) {
-                Log.d(Constants.TAG, "UID: ${user.uid}")
-                Log.d(Constants.TAG, "Name: ${user.displayName}")
-                Log.d(Constants.TAG, "Email: ${user.email}")
-                Log.d(Constants.TAG, "Phone: ${user.phoneNumber}")
-                Log.d(Constants.TAG, "Photo URL: ${user.photoUrl}")
-
-                switchToPublicForumFragment(user.uid)
-            } else {
-                switchToSplashFragment()
-            }
-        }
-    }
-
-    private fun switchToSplashFragment() {
-        supportActionBar?.hide()
-        val ft = supportFragmentManager.beginTransaction()
-        ft.replace(R.id.fragment_container, SplashFragment())
-        ft.commit()
-    }
-
-    private fun switchToPublicForumFragment(uid: String) {
-        supportActionBar?.show()
-        val ft = supportFragmentManager.beginTransaction()
-        ft.replace(R.id.fragment_container, PublicForumFragment.newInstance(uid))
-        ft.commit()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        auth.addAuthStateListener(authStateListener)
-    }
-
-    override fun onStop() {
-        super.onStop()
-        auth.removeAuthStateListener(authStateListener)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -105,41 +53,5 @@ class MainActivity : AppCompatActivity(),
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
-
-    override fun onRosefireLoginButtonPressed() {
-        launchLoginUI()
-    }
-
-    private fun launchLoginUI() {
-        val REGISTRY_TOKEN = getString(R.string.rosefire_token)
-        val signInIntent: Intent = Rosefire.getSignInIntent(this, REGISTRY_TOKEN)
-        startActivityForResult(signInIntent, RC_ROSEFIRE_LOGIN)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK && requestCode == RC_ROSEFIRE_LOGIN) {
-            val result = Rosefire.getSignInResultFromIntent(data)
-            if (result.isSuccessful) {
-                auth.signInWithCustomToken(result.token)
-                Log.d(Constants.TAG, "Username: ${result.username}")
-                Log.d(Constants.TAG, "Name: ${result.name}")
-                Log.d(Constants.TAG, "Email: ${result.email}")
-                Log.d(Constants.TAG, "Group: ${result.group}")
-            } else {
-                Log.d(Constants.TAG, "Rosefire failed")
-            }
-        }
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.action_settings -> {
-                auth.signOut()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 }

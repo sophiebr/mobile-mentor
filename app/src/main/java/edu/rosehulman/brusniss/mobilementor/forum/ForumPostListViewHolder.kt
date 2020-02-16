3a4aad77b260ce046f53d21126b2c7e2f9ac1683
Rias A Hilliard <hilliara@rose-hulman.edu>
2020-02-16 12:36:36 -0500

    Forum post overview bound properly

--- app/src/main/java/edu/rosehulman/brusniss/mobilementor/forum/ForumPostListViewHolder.kt
+++ app/src/main/java/edu/rosehulman/brusniss/mobilementor/forum/ForumPostListViewHolder.kt
@@ -1,20 +1,25 @@
 package edu.rosehulman.brusniss.mobilementor.forum
 
+import android.provider.SyncStateContract
+import android.util.Log
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.fragment_forum_post.view.*
+import edu.rosehulman.brusniss.mobilementor.Constants
+import edu.rosehulman.brusniss.mobilementor.R
 import kotlinx.android.synthetic.main.fragment_forum_row.view.*
-import kotlinx.android.synthetic.main.fragment_forum_row.view.post_title_text
-import kotlinx.android.synthetic.main.fragment_forum_row.view.post_like_text
-import kotlinx.android.synthetic.main.fragment_forum_row.view.post_star_text
 
 class ForumPostListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun bind(model: ForumPostModel) {
         itemView.post_title_text.text = model.title
-//        itemView.post_question_text.text = model.content
         itemView.post_like_text.text = model.likeCount.toString()
-//        itemView.post_star_text.text = model.mentorResponseCount.toString()
-//        itemView.post_edit_text.text = model.responseCount.toString()
+        itemView.post_star_text.text = model.mentorResponseCount.toString()
+        itemView.post_edit_text.text = model.responseCount.toString()
+        
+        itemView.post_response_status.setImageResource(when (model.questionState) {
+            QuestionStatus.MENTOR_ANSWERED -> R.drawable.ic_answered
+            QuestionStatus.NOTIFICATION -> R.drawable.ic_forum_notification
+            else -> R.drawable.ic_hourglass
+        })
     }
 }

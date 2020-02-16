c6eb8bbfe0ab0efbebf1afbdf4d2cca5528ee831
Rias A Hilliard <hilliara@rose-hulman.edu>
2020-02-16 12:25:25 -0500

    Title binds to view

--- app/src/main/java/edu/rosehulman/brusniss/mobilementor/forum/ForumPostListViewHolder.kt
+++ app/src/main/java/edu/rosehulman/brusniss/mobilementor/forum/ForumPostListViewHolder.kt
@@ -2,10 +2,19 @@ package edu.rosehulman.brusniss.mobilementor.forum
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_forum_post.view.*
+import kotlinx.android.synthetic.main.fragment_forum_row.view.*
+import kotlinx.android.synthetic.main.fragment_forum_row.view.post_title_text
+import kotlinx.android.synthetic.main.fragment_forum_row.view.post_like_text
+import kotlinx.android.synthetic.main.fragment_forum_row.view.post_star_text
 
 class ForumPostListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun bind(model: ForumPostModel) {
-
+        itemView.post_title_text.text = model.title
+//        itemView.post_question_text.text = model.content
+//        itemView.post_like_text.text = model.likeCount.toString()
+//        itemView.post_star_text.text = model.mentorResponseCount.toString()
+//        itemView.post_edit_text.text = model.responseCount.toString()
     }
 }

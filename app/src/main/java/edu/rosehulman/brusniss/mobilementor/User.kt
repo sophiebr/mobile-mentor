b59a10ae31a5c6bc5c0596a8ec41c3893d5ceb4c
Rias A Hilliard <hilliara@rose-hulman.edu>
2020-02-17 22:36:39 -0500

    Removed unnecessary fields

--- app/src/main/java/edu/rosehulman/brusniss/mobilementor/User.kt
+++ app/src/main/java/edu/rosehulman/brusniss/mobilementor/User.kt
@@ -6,16 +6,13 @@ import edu.rosehulman.brusniss.mobilementor.profile.ProfileModel
 import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 object User {
-    var name: String = ""
     @PermissionLevel var permissionLevel: Int = 0
-    var pictureUrl: String = ""
     var firebasePath: String = ""
 
     fun set(firebasePath: String) {
         val userRef = FirebaseFirestore.getInstance().document(firebasePath)
         userRef.get().addOnSuccessListener {
             val user = ProfileModel.fromSnapshot(it)
-            name = user.name
             permissionLevel = user.permissionLevel
             this.firebasePath = firebasePath
         }

cd9c5d053ccd3fe6675904c6ed32b5f10221ddc3
Rias A Hilliard <hilliara@rose-hulman.edu>
2020-02-17 23:38:44 -0500

    Responses show names now

--- app/src/main/java/edu/rosehulman/brusniss/mobilementor/forum/ForumResponseViewHolder.kt
+++ app/src/main/java/edu/rosehulman/brusniss/mobilementor/forum/ForumResponseViewHolder.kt
@@ -1,7 +1,11 @@
 package edu.rosehulman.brusniss.mobilementor.forum
 
+import android.provider.SyncStateContract
+import android.util.Log
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import edu.rosehulman.brusniss.mobilementor.Constants
+import edu.rosehulman.brusniss.mobilementor.profile.ProfileModel
 import kotlinx.android.synthetic.main.fragment_forum_post_response_row.view.*
 
 class ForumResponseViewHolder(itemView: View, private var adapter: ForumResponseAdapter) : RecyclerView.ViewHolder(itemView) {
@@ -15,5 +19,10 @@ class ForumResponseViewHolder(itemView: View, private var adapter: ForumResponse
     fun bind(model: ForumResponseModel) {
         itemView.post_response_text.text = model.content
         itemView.post_like_text.text = model.likes.toString()
+        model.author?.get()?.addOnSuccessListener {
+            val profile = ProfileModel.fromSnapshot(it)
+            itemView.post_author_name.text = profile.name
+            //TODO: picture
+        }
     }
 }
